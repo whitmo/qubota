c7b7ad2445594f3098b76b7af67d55a1a142a119
whitmo <d.w.morriss@gmail.com>
2012-12-13 15:43:51 -0600

    add id printing to test job

--- qubota/tests/__init__.py
+++ qubota/tests/__init__.py
@@ -3,10 +3,11 @@ import logging
 
 log = logging.getLogger(__name__)
 
-def simple_job(howlong=240.0, errout=False, interval=10):
+def simple_job(howlong=10, errout=False, interval=1):
     """
     This job wastes cpu cycles for the length of 'howlong'
     """
+    print simple_job.uid
     with gevent.Timeout(float(howlong), errout):
         log.warning("I'm awake")
         while True:
