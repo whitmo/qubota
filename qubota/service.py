3c8f8672f2ee0467fc77c80a2ba1d310cfaf5ee7
whitmo <d.w.morriss@gmail.com>
2012-12-12 20:00:39 -0600

    hack out internal arbiter

--- qubota/service.py
+++ qubota/service.py
@@ -1,21 +1,23 @@
 from . import sqs
+from . import utils
 from .job import Job
-from .utils import resolve
 from .utils import reify
+from .utils import resolve
 from .zmqutils import Context
 from .zmqutils import zmq
-from circus import get_arbiter
+from circus.client import CircusClient
 from ginkgo import Service
 from ginkgo import Setting
 from stuf import stuf
 import ginkgo
 import logging
+import os
+import signal
 import sys
 import time
 import traceback
 import uuid
-import os
-import signal
+
 
 
 class QService(Service):
@@ -37,6 +39,15 @@ class QService(Service):
         self.socks = stuf()
         self._id = uuid.uuid4().hex
         self.ctx = Context.instance()
+
+
+"""
+#TODO
+# reimplement
+- upstart circus
+- initial watcher definition
+- incr when new job is reserved
+"""
 
 
 class Drain(QService):
@@ -61,44 +72,63 @@ class Drain(QService):
 
     num_workers = Setting('num_workers', default=10)
 
-    get_arbiter = staticmethod(get_arbiter)
-
     def __init__(self, config=None):
         super(Drain, self).__init__(config)
         self.reservations = {}
 
+    @classmethod
+    def ctor(cls, yaml):
+        return cls(yaml)
+
     @reify
-    def drone_watch(self):
-        #staling?
-        self.arbiter.get_watcher(self.prefix)
+    def cclient(self):
+        #@@ add ssh support??
+        self.log.info("Circus client: %s" %self.circus_endpoint)
+        return CircusClient(endpoint=self.circus_endpoint)
+
+    @staticmethod
+    def ccmd(command, **props):
+        return dict(command=command, properties=props)
+
+    # @reify
+    # def drone_watch(self):
+    #     #staling?
+    #     return self.arbiter.get_watcher(self.prefix)
 
     @reify
     def watcher_def(self):
-        return dict(name=self.prefix, cmd='qubota drone',
-                    args="--endpoint={} --queue={}"\
+        return dict(name=self.prefix, 
+                    cmd="bash -l -c 'qb drone --endpoint={} --queue={}'"\
                         .format(self.endpoint, self.queue.name),
+                    shell=True,
                     numprocesses=self.num_workers)
 
     def do_start(self):
         self.log.info("Starting %s: pid: %s" %(self.__class__.__name__, os.getpid()))
-        self.arbiter = get_arbiter([self.watcher_def], 
-                                   context=self.ctx,
-                                   proc_name=self.proc_name,
-                                   stream_backend='gevent')
+
+        # self.arbiter = get_arbiter([self.watcher_def], 
+        #                            context=self.ctx,
+        #                            proc_name=self.proc_name,
+        #                            stream_backend='gevent')
+
         signal.signal(signal.SIGINT, self.signal)
         signal.signal(signal.SIGTERM, self.signal)
-        self.spawn(self.worker_loop)
-        self.spawn(self.listener)
+        # self.arbgr = self.tpool.spawn(self.arbiter.start)
+        self.async.spawn(self.worker_loop)
+        self.async.spawn(self.listener)
+
+
+
 
     def do_stop(self):
-        if self.arbiter.alive:
-            self.arbiter.stop_watchers(stop_alive=True)
+        # if self.arbiter.alive:
+        #     self.arbiter.stop_watchers(stop_alive=True)
 
-        self.arbiter.loop.stop()
+        # self.arbiter.loop.stop()
 
-        # close sockets
-        self.arbiter.sockets.close_all()        
-
+        # # close sockets
+        # self.arbiter.sockets.close_all()        
+        pass
 
     def signal(self, *args):
         self.log.critical("SIG %s - exiting", args[0])
@@ -106,7 +136,9 @@ class Drain(QService):
 
     def listener(self):
         while self.running:
+            self.log.debug('listen')
             msg = self.dealer.recv()
+            self.log.debug(msg)
             self.process_msg(msg)
             self.async.sleep(self.wait_interval)
 
@@ -117,16 +149,17 @@ class Drain(QService):
     def running(self):
         return self.state.current in ['starting', 'ready']
 
-    @property
-    def worker_watchers(self):
-        return (watch for watch in self.arbiter.iter_watchers \
-                    if watch.name.startswith(self.prefix))
+    # @property
+    # def worker_watchers(self):
+    #     return (watch for watch in self.arbiter.iter_watchers \
+    #                 if watch.name.startswith(self.prefix))
 
     @reify
     def dealer(self):
         """
         lazy load dealer socket
         """
+        self.log.info("Dealer initialized at: %s", self.endpoint)
         dealer = self.ctx.dealer(bind=self.endpoint)
         dealer.setsockopt(zmq.LINGER, 0)
         return dealer
@@ -136,18 +169,19 @@ class Drain(QService):
         pass
 
     def reserve_job(self, msg):
-        #@@ error catching
-        job = Job.from_map(msg.get_body())
-        job.update_state('RESERVED')
-        job.last_modified = time.time()
-        self.domain.put_attributes(job.id, job)
-        self.reserved[job.id] = job        
-        self.queue.delete_message(msg)
-        return job
+        with utils.log_tb(self.log, True):
+            job = Job.from_map(msg.get_body())
+            job.update_state('RESERVED')
+            job.last_modified = time.time()
+            self.domain.put_attributes(job.id, job)
+            self.reserved[job.id] = job        
+            self.queue.delete_message(msg)
+            return job
 
     def dispatch(self, job):
         # one out, one in
-        self.drone_watcher.incr()
+        #self.drone_watcher.incr()
+        #@@ replace with client call
         self.dealer.send_json(dict(job))
         job.update_state('DISPATCHED')
 
@@ -158,10 +192,14 @@ class Drain(QService):
         - wait for ack / note who grabbed it
         - monitor worker pool / restart if necessary
         """
+        start = time.time()
         while True:
+            if int(start) % 100 == 0:
+                self.log.debug('wake up: check msgs')
             for msg, job in sqs.msgs(self.queue, num=self.num_workers):
                 self.async.spawn(self.reserve_job, job).link(self.dispatch)
             self.async.sleep(self.poll_interval)            
+            
 
 
 class Drone(QService):
@@ -208,6 +246,8 @@ class Drone(QService):
         self.log.info("Starting %s: pid: %s" %(self.__class__.__name__, os.getpid()))
         self.drain = self.ctx.dealer(connect=self.drain)
         self.async.spawn(self.work)
+
+
 
 
 
