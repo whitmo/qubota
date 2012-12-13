4bbe87abe90d969ba8c2f99e0f02d26c8971a23c
whitmo <d.w.morriss@gmail.com>
2012-12-13 00:51:40 -0600

    hey... running test job. do some logging, do some print, blah blah

--- qubota/tests/__init__.py
+++ qubota/tests/__init__.py
@@ -1,14 +1,20 @@
 import gevent
+import logging
 
+log = logging.getLogger(__name__)
 
-def simple_job(howlong=240, errout=False, interval=10):
+def simple_job(howlong=240.0, errout=False, interval=10):
     """
     This job wastes cpu cycles for the length of 'howlong'
     """
-    with gevent.Timeout(howlong, errout)):
+    with gevent.Timeout(float(howlong), errout):
+        log.warning("I'm awake")
         while True:
             gevent.sleep(interval)
-        
+            print "Makin' donuts"
+    log.critical("And I'm out...")
+            
+
 
 
 
