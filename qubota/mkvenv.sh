878d962dda025b3776f78e7420f22614a695fdc5
whitmo <d.w.morriss@gmail.com>
2012-12-13 00:55:25 -0600

    move out from runcmd

---
+++ qubota/mkvenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+wget https://raw.github.com/whitmo/qubota/master/req.txt -O /tmp/req.txt
+chmod 666 /tmp/req.txt
+chown -R ec2-user:ec2-user /home/ec2-user/app/qubota
+su ec2-user -c "bash -l -c 'mkvirtualenv --python=python27 /home/ec2-user/app/qubota'"
+su ec2-user -c "/bin/bash -l -c 'workon qubota && pip install -r /tmp/req.txt'"
+start qb-circusd
+sleep 2s
+status qb-circusd
+echo DONE > /tmp/mkenv-done
