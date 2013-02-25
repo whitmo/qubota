ab0f649b9be04d002ea7bd1b914923fb7134b5d5
whitmo <d.w.morriss@gmail.com>
2013-02-25 11:48:18 -0600

    bad request test (404)

--- qubota/tests/test_webhook.py
+++ qubota/tests/test_webhook.py
@@ -2,8 +2,10 @@ from .patch_requests import install_opener
 from .patch_requests import uninstall_opener
 from mock import Mock
 from mock import patch
-from webob.dec import wsgify
+from nose.tools import raises
 from webob import Response
+from webob.dec import wsgify
+from webob.exc import HTTPNotFound
 import unittest
 import wsgi_intercept
 
@@ -55,7 +57,17 @@ class TestWebhook(unittest.TestCase):
         wsgi_intercept.add_wsgi_intercept('target', 80, lambda : self.target)
         self.make_one()("PUT http://target:80", data=dict(hi='there'))
         assert self.current_request.body == '{"hi": "there"}'
-        
+
+
+    def test_bad_target_responese(self):
+        wsgi_intercept.add_wsgi_intercept('target', 80, lambda : self.target)
+        with patch('qubota.tests.test_webhook.TestWebhook.the_response') as res_call:
+            res_call.return_value = HTTPNotFound()
+            from qubota.jobs.webhook import WebHookHTTPFailure
+            try:
+                self.make_one()("PUT http://target:80", data=dict(hi='there'))
+            except WebHookHTTPFailure, e:
+                assert e.message == '404 Not Found'
 
 
 
