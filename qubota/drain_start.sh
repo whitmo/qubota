700ef0228929b58aacf8839a2ba822c4798a1c34
whitmo <d.w.morriss@gmail.com>
2013-03-07 11:00:23 -0600

    update source first

--- qubota/drain_start.sh
+++ qubota/drain_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash  
+su ec2-user -c "bash -l -c cd /home/ec2-user/app/qubota/src/qubota && git pull --rebase origin master "
 start drain
 sleep 4s
 status drain
 tail /var/log/drain.log 
-su ec2-user -c "bash -l -c cd /home/ec2-user/app/qubota/src/qubota && git pull --rebase origin master "
+
