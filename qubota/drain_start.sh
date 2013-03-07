665f696ea6d18c7dbf053fcbb80115d95c6371d7
whitmo <d.w.morriss@gmail.com>
2013-03-07 11:00:04 -0600

    hijack dstart to update qubota source

--- qubota/drain_start.sh
+++ qubota/drain_start.sh
@@ -2,4 +2,5 @@
 start drain
 sleep 4s
 status drain
-tail /var/log/drain.log
+tail /var/log/drain.log 
+su ec2-user -c "bash -l -c cd /home/ec2-user/app/qubota/src/qubota && git pull --rebase origin master "
