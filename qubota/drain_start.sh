ca7c166601a40b422489592c075f5aaf7ac68e56
whitmo <d.w.morriss@gmail.com>
2013-03-18 12:44:51 -0700

    some changes to attempt to debug why rebasing is not happening

--- qubota/drain_start.sh
+++ qubota/drain_start.sh
@@ -2,6 +2,6 @@
 su ec2-user -c "bash -l -c cd /home/ec2-user/app/qubota/src/qubota && git pull --rebase origin master "
 start drain
 sleep 4s
-status drain
-tail /var/log/drain.log 
+status drain > /init-report.log
+tail /var/log/drain.log >> /init-report.log
 
